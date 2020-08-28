8a478312a5b3523e3696ce3623dc892b16a54a85
Jack Ladwig <jladwig@sift.net>
2020-08-28 12:53:02 -0500

    remove bitrotting example message

--- actors/xplan_coordinate/xplan_coordinate_reactor/src/xplan_coordinate_reactor/messagetypes/__init__.py
+++ actors/xplan_coordinate/xplan_coordinate_reactor/src/xplan_coordinate_reactor/messagetypes/__init__.py
@@ -1,5 +1,4 @@
 from .abacomessage import *
-from .examplemessage import ExampleMessage, ExampleMessageError
 from .jobcompletionmessage import JobCompletionMessage
 from .xplandesignmessage import XPlanDesignMessage, XPlanDesignMessageError
 from .filemessage import FileMessage, FileMessageError
