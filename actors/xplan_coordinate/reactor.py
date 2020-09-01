17b376fb5db8090c3b8a1cf5f0cc53be6bfa05b0
Jack Ladwig <jladwig@sift.net>
2020-09-01 14:24:41 -0500

    extra handling of job response messages.
    This should help with debugging if a job sends a message with an
    unexpected status (like BLOCKED). We are not actually registering
    for those messages but I have seen them arrive after a job ended
    up BLOCKED. So it is possible that the webhook handlers need some
    extra code to correct the execution if a non-FINISHED status msg
    arrives.

--- actors/xplan_coordinate/reactor.py
+++ actors/xplan_coordinate/reactor.py
@@ -3,25 +3,45 @@
 from attrdict import AttrDict
 from xplan_coordinate_reactor import typed_message_from_context, typed_message_from_dict, as_job_completion_message
 from xplan_coordinate_reactor.jobs import deregister_job, num_jobs
+from xplan_coordinate_reactor.messages import JobCompletionMessage
 from reactors.runtime import Reactor
 from xplan_utils import persist
 import os
+
+
+def process_job_message(r: Reactor, job: JobCompletionMessage):
+    job_id = job.get('id')
+    status = job.get('status')
+
+    if status == "FINISHED":
+        r.logger.debug("Processing completed job with id " + job_id)
+        raw_msg = deregister_job(r, job_id)
+        if raw_msg is None:
+            r.logger.debug(
+                "Failed to find message associated with job id: " + job_id)
+            return
+
+        msg = typed_message_from_dict(raw_msg)
+        r.logger.debug("Finalizing message of type " + type(msg).__name__)
+        msg.finalize_message(r, job)
+    elif status == "BLOCKED":
+        # TODO it seems that jobs will respond to FINISHED webhooks with BLOCKED events.
+        # Catch that case here so I can debug if it happens again. It may require
+        # that a fresh webhook be registered for the job that blocked.
+        # For now do the same as any other unexpected status message...
+        r.logger.error(
+            "Received {} message for job {}.".format(status, job_id))
+    else:
+        # Similar to BLOCKED but just in case we get any other status
+        r.logger.error(
+            "Received {} message for job {}.".format(status, job_id))
 
 
 def main():
     r = Reactor()
     job = as_job_completion_message(r.context)
     if job is not None:
-        job_id = job.get('id')
-        r.logger.debug("Processing completed job with id " + job_id)
-        raw_msg = deregister_job(r, job_id)
-        if raw_msg is None:
-            r.logger.debug(
-                "Failed to find message associated with job id: " + job_id)
-        else:
-            msg = typed_message_from_dict(raw_msg)
-            r.logger.debug("Finalizing message of type " + type(msg).__name__)
-            msg.finalize_message(r, job)
+        process_job_message(r, job)
     else:
         msg = typed_message_from_context(r.context)
         r.logger.debug("Processing message of type " + type(msg).__name__)
