09c5ba07a4c5db5ce56dd414be0bb212946cd084
Daniel Bryce <danbryce410@gmail.com>
2020-09-02 14:15:21 -0500

    fix rebased script

--- scripts/run_reactor.sh
+++ scripts/run_reactor.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-REMOTE_WORKDIR=$1
-REACTOR_NAME=$2
-
-#tapis files mkdir agave://${REMOTE_WORKDIR} xplan2/test/resources
-#tapis files upload agave://${REMOTE_WORKDIR}/xplan2/test/resources components/xplan_design/test/resources/request_experiment.transcriptic.2020-05-04-YeastSTATES-1-0-Growth-Curves.json
-
-#tapis files mkdir agave://${REMOTE_WORKDIR}/xplan2 secrets
-#tapis files upload agave://${REMOTE_WORKDIR}/xplan2/secrets secrets/tx_secrets.json
-
-#tapis files mkdir agave://${REMOTE_WORKDIR}/xplan2 out
-
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ACTOR_ID=`tapis actors list | grep ${REACTOR_NAME} | cut -f 2 -d "|"`
 
