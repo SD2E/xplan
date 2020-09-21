1aef305f3a7c11349dbc38ae034c8a56923b9800
Jack Ladwig <jladwig@sift.net>
2020-09-21 14:51:57 -0500

    test case for creating a container

--- components/xplan_coordinate/test/unit/test_create_container.py
+++ components/xplan_coordinate/test/unit/test_create_container.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 import sys
-from xplan_utils.lab.strateos.containers import generate_containers, query_protocol
+from xplan_utils.lab.strateos.containers import make_containers
 
 transcriptic_params = {
     "default": "XPlanAutomatedExecutionTest",
@@ -18,14 +18,27 @@ transcriptic_params = {
 def test_create_container():
     tx_cfg_path = os.path.abspath("../../../../secrets/tx_secrets.json")
     with open(tx_cfg_path, "r") as tx_cfg_file:
+        # Lab Configuration
         tx_cfg = json.load(tx_cfg_file)
-        # query_protocol(tx_cfg, 'pr1eu2avrq8bznx')
-        generate_containers([
+        # XPlanAutomatedExecutionTest project id
+        tx_proj_key = transcriptic_params.get('default')
+        tx_proj = transcriptic_params.get('projects').get(tx_proj_key)
+        tx_proj_id = tx_proj.get('id')
+        # Protocol inputs
+        containers = [
             {
                 "name": "dummy_container_01",
-                "container_type": "conical-15"
+                "cont_type": "micro-1.5",
+                "volume": "1000:microliter",
+                "properties": [
+                    {
+                        "key": "concentration",
+                        "value": "10:millimolar"
+                    }
+                ]
             }
-        ], tx_cfg, test_mode=True)
+        ]
+        make_containers(tx_proj_id, tx_cfg, containers, title='TestCreateContainer', out_dir='../test_out', test_mode=True)
 
 
 def main():
@@ -35,10 +48,10 @@ def main():
     h1.addFilter(lambda record: record.levelno <= logging.INFO)
     h2 = logging.StreamHandler(sys.stderr)
     h2.setLevel(logging.WARNING)
-    logging.basicConfig(handlers = [h1, h2], format='%(levelname)s: %(message)s')
+    logging.basicConfig(handlers=[h1, h2], format='%(levelname)s: %(message)s')
 
     test_create_container()
 
+
 if __name__ == '__main__':
     main()
-
--- components/xplan_utils/src/xplan_utils/lab/strateos/containers.py
+++ components/xplan_utils/src/xplan_utils/lab/strateos/containers.py
@@ -1,16 +1,20 @@
 import json
 import logging
+import os
 import requests
 import transcriptic
+from autoprotocol import Protocol, container_type
 from attrdict import AttrDict
 from transcriptic.jupyter import objects
+from xplan_utils.lab.strateos.submission import submit_to_transcriptic
 from xplan_utils.lab.strateos.utils import TranscripticApiError, get_transcriptic_api, TranscripticRunStatus
+
 
 l = logging.getLogger(__file__)
 l.setLevel(logging.DEBUG)
 
-
 MAKE_CONTAINERS_PROTOCOL = 'pr1eu2avrq8bznx'
+
 
 def _get_headers(tx_cfg):
     # Setup API call
@@ -30,6 +34,9 @@ def _prepare_query_protocols(tx_cfg):
 
 
 def query_protocol(tx_cfg, proto_id):
+    """
+    Search all protocols for the given id
+    """
     (url, headers) = _prepare_query_protocols(tx_cfg)
     l.info("Querying protocols...")
     response = requests.get(url, headers=headers)
@@ -41,47 +48,13 @@ def query_protocol(tx_cfg, proto_id):
     return None
 
 
-def generate_containers(containers, tx_cfg, test_mode=False):
-    """
-    Create a set of containers.
-    """
-
-    l.info("Connect to Transcriptic API")
-    try:
-         conn = get_transcriptic_api(tx_cfg)
-    except Exception as exc:
-        l.error("Failed connecting to Transcriptic")
-        raise TranscripticApiError(exc)
-
-    preview = conn.preview_protocol(MAKE_CONTAINERS_PROTOCOL)
-    l.info("%s", preview)
-
-    # # Setup API call
-    # tx_org_id = tx_cfg.get('organization_id')
-    # tx_email = tx_cfg.get('email')
-    # tx_token = tx_cfg.get('token')
-
-    # # url = "https://secure.transcriptic.com/{}/inventory/samples/create_with_shipment".format(
-    # #     tx_org_id)
-    # # headers = {"X-User-Email": tx_email,  # user-account-email
-    # #            "X-User-Token": tx_token,  # Regular-mode API key
-    # #            "Content-Type": "application/json",
-    # #            "Accept": "application/json"}
-
-    # # Setup Containers
-    # if test_mode is True:
-    #     for container in containers:
-    #         container['test_mode'] = True
-
-    # # Submit container
-    # body = json.dumps({"containers": containers})
-    # l.info("Creating new containers...")
-    # l.debug("Sending: %s", body)
-    # # response = requests.post(url, body, headers=headers)
-    # # resp = json.loads(response.content)
-    # # l.debug("Received Response: %s", resp)
-
-    # # container_id = resp['containers'][0]['id']
-    # # l.info("Created container: %s", container_id)
-
-    # # return container_id
+def make_containers(project_id, tx_cfg, containers, *, title='make_containers', out_dir='.', test_mode=True):
+    tx_proj_id = project_id
+    # Protocol inputs
+    params = {
+        "parameters": {
+            "containers": containers
+        }
+    }
+    submit_to_transcriptic(tx_proj_id, MAKE_CONTAINERS_PROTOCOL, params,
+                           title, out_dir, tx_cfg, test_mode=test_mode)
